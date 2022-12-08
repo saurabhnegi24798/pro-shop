0f15d1887c69ee2e882df752c068c2f617201626
saurabhnegi24798 <saurabhnegi24798@gmail.com>
2022-12-08 17:50:14 +0530

    some changes before editing Order Screen

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -19,7 +19,6 @@ const LoginScreen = ({ location, history }) => {
   const redirect = location.search ? location.search.split('=')[1] : '/'
 
   useEffect(() => {
-    console.log('ha bhai', location);
     if (userInfo) {
       history.push(redirect)
     }
